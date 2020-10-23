8984cd6e58053e59808d2e397f489262bf1aff1d
VINICIUS EDUARDO BATISTA MENDONÇA <vi.edu2014@hotmail.com>
2020-10-22 21:46:05 -0300

    Commit - Projeto MPC - Vinicius Eduardo Batista Mendonça

---
+++ Projeto_Microcontrolador.ino
@@ -0,0 +1,55 @@
+// Autores: Caio Henrique Pardim, Guilherme Hernandes, Silfarny Alexandre, Vinicius Eduardo e Walace Vinicius
+// Projeto referente a avaliação de microcontroladores
+// Professor: Alisson Rodolfo Leite 
+
+int ledvermelho = 0;
+int ledverde = 2;
+int ledamarelo = 1;
+
+void setup() {
+  pinMode (ledvermelho, OUTPUT);
+  pinMode (ledverde, OUTPUT);
+  pinMode (ledamarelo, OUTPUT);
+}
+
+void loop() {
+  digitalWrite (ledvermelho,HIGH);
+  digitalWrite (ledverde,LOW);
+  digitalWrite (ledamarelo,LOW);
+  delay (5000);
+  digitalWrite (ledvermelho,LOW);
+  digitalWrite (ledverde,HIGH);
+  digitalWrite (ledamarelo,LOW);
+  delay (5000);
+  digitalWrite (ledvermelho,LOW);
+  digitalWrite (ledverde,LOW);
+  digitalWrite (ledamarelo,HIGH);
+  delay (2500);
+
+  digitalWrite (ledvermelho,HIGH);
+  digitalWrite (ledverde,LOW);
+  digitalWrite (ledamarelo,LOW);
+  delay (10000);
+  digitalWrite (ledvermelho,LOW);
+  digitalWrite (ledverde,HIGH);
+  digitalWrite (ledamarelo,LOW);
+  delay (10000);
+  digitalWrite (ledvermelho,LOW);
+  digitalWrite (ledverde,LOW);
+  digitalWrite (ledamarelo,HIGH);
+  delay (4000);
+
+  digitalWrite (ledvermelho,HIGH);
+  digitalWrite (ledverde,LOW);
+  digitalWrite (ledamarelo,LOW);
+  delay (6000);
+  digitalWrite (ledvermelho,LOW);
+  digitalWrite (ledverde,HIGH);
+  digitalWrite (ledamarelo,LOW);
+  delay (6000);
+  digitalWrite (ledvermelho,LOW);
+  digitalWrite (ledverde,LOW);
+  digitalWrite (ledamarelo,HIGH);
+  delay (3000);
+
+}
